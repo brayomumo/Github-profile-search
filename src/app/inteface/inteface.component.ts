0f71ebe3abe8478b2281d23c3d0e29e8c67c2286
brian <brayomumo>
2019-07-14 16:02:52 +0300

    Link to the Api

--- src/app/inteface/inteface.component.ts
+++ src/app/inteface/inteface.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
-import { User } from '../models/user.model';
+import { Component, OnInit, ComponentFactory } from '@angular/core';
+import { GitServe } from '../git-serv.service';
+import { User } from '../user';
+import { Repos } from '../repositories';
+
 
 @Component({
   selector: 'app-inteface',
   templateUrl: './inteface.component.html',
-  styleUrls: ['./inteface.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./inteface.component.css']
 })
 export class InterfaceComponent implements OnInit {
+  username:string;
+  profile:User;
+  repos:Repos
+  constructor(private profileService: GitServe) {
 
-  constructor() { }
-  @Input() user: User;
+  }
+
+  findProfile(){
+  	this.profileService.updateProfile(this.username)
+  	this.profileService.getProfileInfo().subscribe(profile => {
+  		console.log(profile);
+  		this.profile = profile;
+  	})
+
+  	this.profileService.getProfileRepos().subscribe(repos => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})
+  }
+
   ngOnInit() {
+    interface ApiResponse{
+    name: string;
+    login: string;
+    avatar_url: any;
+    html_url: string;
+    public_repos: string;
+    }
   }
 
 }
-
