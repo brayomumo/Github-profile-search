52a70ffc4f0d2a973d4c50a07224aa771e837db5
brian <brayomumo>
2019-07-14 07:56:25 +0300

    Add module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IntefaceComponent } from './inteface/inteface.component';
+import { InterfaceComponent } from './inteface/inteface.component';
 import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntefaceComponent,
+    InterfaceComponent,
     HomeComponent
   ],
   imports: [
