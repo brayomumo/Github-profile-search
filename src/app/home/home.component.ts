33d362cf119182312668e7a75f2d0b64c55bfafd
brian <brayomumo>
2019-07-14 16:01:38 +0300

    Discard this component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { GitService } from '../git-serv.service';
-import { User } from '../models/user.model';
-import { filter, switchMap, debounceTime, catchError } from 'rxjs/operators';
-import { EMPTY } from 'rxjs';
+// import { Component, OnInit } from '@angular/core';
+// import { FormControl } from '@angular/forms';
+// import { GitServe } from '../git-serv.service';
+// import { User } from '../user';
+// import { Observable } from 'rxjs';
+// import { filter, switchMap, debounceTime, catchError } from 'rxjs/operators';
+// import { EMPTY } from 'rxjs';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
-export class HomeComponent implements OnInit {
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements OnInit {
 
 
-  constructor(private githubService: GitService) { }
-  findControl = new FormControl();
-  error: boolean = false;
-  user: User = null;
-  ngOnInit() {
-    this.findControl.valueChanges
-      .pipe(filter(value => value.length > 3),
-      debounceTime(1000),
-      switchMap(value =>
-        this.githubService.getUser(value).pipe(
-          catchError(err => {
-            this.user = null;
-            this.error = true;
-            return EMPTY;
-          })))
-      ).subscribe(user => {
-        this.user = user;
-        this.error = false;
-      });
-  }
+//   constructor(private githubService: GitServe) { }
+//   findControl = new FormControl();
+//   error: boolean = false;
+//   user: User = null;
+//   ngOnInit() {
+//     this.findControl.valueChanges
+//       .pipe(filter(value => value.length > 3),
+//       debounceTime(1000),
+//       switchMap(value =>
+//         this.githubService.getUser(value).pipe(
+//           catchError(err => {
+//             this.user = null;
+//             this.error = true;
+//             return EMPTY;
+//           })))
+//       ).subscribe(user => {
+//         this.user = user;
+//         this.error = false;
+//       });
+//   }
 
-}
+// }
