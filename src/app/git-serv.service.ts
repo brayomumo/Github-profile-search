940e91d157a71458240b19401fd0ca0d72a7e6dd
brian <brayomumo>
2019-07-14 07:53:30 +0300

    add a service

--- src/app/git-serv.service.ts
+++ src/app/git-serv.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GitServService {
-  private username: string;
-  private clientId = '1560ae83c3ec6ed02792';
-  private ClientSecret = 'f6eeacbb082b686c6103070652c2db1ae2ac7797';
+export class GitService {
 
-  constructor(private http: Http ) {
-    console.log('Service is now ready!');
-    this.username = 'brayomumo';
-   }
-   getProfileInfo() {
-    // tslint:disable-next-line: max-line-length
-    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientId + '&client_secret' + this.ClientSecret).forEach(res => res.json());
-   }
+  constructor(private http: HttpClient) { }
+  getUser(name: string): Observable<User> {
+    const url = `https://api.github.com/users/${name}`;
+    return this.http.get<User>(url);
+  }
 }
