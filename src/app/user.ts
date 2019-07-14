78a034be4a02e2c9bc7cde7c35c13fe414209330
brian <brayomumo>
2019-07-14 16:01:01 +0300

    Create a class to host user details

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  name: string;
+   login: string;
+   avatar_url: any;
+   // followers_url: number;
+   // following_url: number;
+   public_repos: string;
+   // created_at: string;
+   html_url: string;
+}
